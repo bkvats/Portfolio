c219577b3a273e93017cdae4315891a76a43790a
bkvats <bkvats2394@gmail.com>
2024-08-12 23:33:14 +0530

    trying through public folder

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -4,7 +4,7 @@ export default function Intro() {
     return (
         <div className="w-full flex flex-col justify-evenly items-center mt-20 lg:flex-row">
             <div>
-                <img src="./bg-portfolioimage.jpg" alt="bimage" className="rounded-full mx-2 w-[300px] lg:w-[500px]" />
+                <img src="/bg-portfolioimage.jpg" alt="bimage" className="rounded-full mx-2 w-[300px] lg:w-[500px]" />
             </div>
             <div>
                 <h1 className="text-4xl font-semibold m-7">I'm Software Developer skilled in <span className="text-[#C778DD]">DSA</span> and <span className="text-[#C778DD]">Full-Stack Development</span></h1>
