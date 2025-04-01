ce4a76253e2f253945302ffe55b9bad5d68a4030
Bhupender Kumar Sharma <145216160+bkvats@users.noreply.github.com>
2025-04-01 14:52:03 +0530

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -11,7 +11,7 @@ export default function Intro() {
                 <h1 className="text-4xl font-semibold m-7">I'm Software Developer skilled in <span className="text-[#C778DD]">DSA</span> and <span className="text-[#C778DD]">Full-Stack Development</span></h1>
                 <p className="text-[#ABB2BF] m-7">I design and develop responsive web applications where technologies meet creativity</p>
                 <div className="lg:ml-7 flex flex-wrap items-center justify-evenly lg:w-fit lg:gap-4">
-                    <a href="https://drive.google.com/file/d/1mZnP4bGe0hOmX4gUPilfliElLx9FXxiu/view?usp=sharing" className="flex items-center gap-2 border-2 border-[#C778DD] py-2 px-4" download={"BSharmaResume"} target="_blank">< ImDownload/>Download CV</a>
+                    <a href="https://drive.google.com/file/d/1Y0CcDMeWTE5-fjApFUwHeLuUz_6wvHvd/view?usp=sharing" className="flex items-center gap-2 border-2 border-[#C778DD] py-2 px-4" download={"BSharmaResume"} target="_blank">< ImDownload/>Download CV</a>
                     <Link to="/contacts" className="border-2 border-[#ABB2BF] py-2 px-4 text-[#ABB2BF]">Contact me !!</Link>
                 </div>
             </div>
